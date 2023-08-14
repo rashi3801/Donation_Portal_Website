e27a47eeece76f2a0479a4f5b768a20f3a945e90
rashi3801 <102690434+rashi3801@users.noreply.github.com>
2023-08-15 00:58:25 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Donation Portal</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="landing">
+
+			<header id="header" class="alt">
+				<h1 ><strong><a href="home.php" >Donation Portal - </a></strong>   Sparks Foundation</h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="#one">About</a></li>
+						<li><a href="#two">Contact</a></li>
+						<li><a href="index.php">Logout</a></li>
+					</ul>
+				</nav>
+			</header>
+
+			<a href="#menu" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+
+			<section id="banner">
+				<h2>Welcome To Sparks Donation Portal!</h2>
+				<p>We are Giving things for happiness <br /> Do a good deed, give to those in need.</p>
+			
+				<ul class="actions">
+					<form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_Go5PzPgaRSQmlj" async> </script> </form>
+				</ul>
+			</section>
+
+			<!-- One -->
+				<section id="one" class="wrapper style1">
+					<div class="container 75%">
+						<div class="row 200%">
+							<div class="6u 12u$(medium)">
+								<header class="major">
+									<h2>Sparks Donation</h2>
+									<p>Every Good Act Counts</p>
+								</header>
+							</div>
+							<div class="6u$ 12u$(medium)">
+								<p style="font-weight: bold; font-style: italic;">We are going with an vision to contribute people who really need essentials to survive this life, In order to make their life better so that little contribution will lead to tremendous change one day to accomplish their dreams. So that you can become the part of this little help and contribute to make their life significant.</p>
+								<p style="font-weight: bold; font-style: italic;">Please-Note : This is just for a DEMO PORTAL which is build as an Internship tasks By SPARKS FOUNDATION.The Money you will contribute in donation that will be only for testing purpose. </p>
+							</div>
+						</div>
+					</div>
+				</section>
+
+			<!-- two -->
+				<section id="two" class="wrapper style3 special">
+					<div class="container">
+						<header class="major">
+							<h2>Sparks Foundation Internship</h2>
+							<p>Piyush Jain</p>
+						</header>
+						<ul class="actions">
+							<li><a href="https://www.linkedin.com/in/piyush-jain-248101180/" class="button special big">Get in touch</a></li>
+						</ul>
+					</div>
+				</section>
+
+		<!-- Logos -->
+			<footer id="footer">
+				<div class="container">
+					<ul class="icons">
+						<li><a href="https://github.com/piyushjain-hue" class="icon fa-github"></a></li>
+						<li><a href="https://www.linkedin.com/in/piyush-jain-248101180/" class="icon fa-linkedin"></a></li>
+					</ul>
+					<ul class="copyright">
+						<li style="font-weight: bold;">&copy; No Copyright Issue, Just Ping Me Anytime @Piyush Jain</li>
+					</ul>
+				</div>
+			</footer>
+
+	</body>
+</html>
